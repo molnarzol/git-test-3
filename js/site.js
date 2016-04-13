9b9ccc0d5a4c553ba2cc07d144c786f304a3a229
Zoltan Molnar <molnarz1989@gmail.com>
2016-04-13 12:49:15 +0200

    build readty to check

--- js/site.js
+++ js/site.js
@@ -4,4 +4,12 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    var cw = $('.item').width();
+    $('.item').css({'height':cw+'px'});
+
+    $( window ).resize(function() {
+        console.log('resize');
+        var cw = $('.item').width();
+        $('.item').css({'height':cw+'px'});
+    });
 });
